117448410fca5a237cd9170b6fc4f9ccf8b2f75e
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-04-21 16:50:07 +0530

    Fsm project intialised

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameController
+{
+    private static GameController instance;
+    public static GameController Instance {
+        get
+        { if (instance == null)
+            {
+                instance = new GameController();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+        return instance;
+        }
+           
+    } 
+    private List<GameObject> checkpoints=new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+    // Start is called before the first frame update
+   
+}
+
